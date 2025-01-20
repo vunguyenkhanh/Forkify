d00c6f9fb4c7cc146ea5f5337ccdefadd23cfd31
vunguyenkhanh <vunguyenkhanh.6599@gmail.com>
2025-01-20 10:20:51 +0700

    add new views and helper functions for recipe search and pagination; update dependencies

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '56614eeb-be36-494b-b0e5-b6a3e683070b';
+export const MODAL_CLOSE_SEC = 2.5;
